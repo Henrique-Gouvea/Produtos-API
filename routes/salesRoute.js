bf0087a546672fe31c27c58ab27074795b21cd69
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-06-29 15:08:20 -0300

    feat: create principal route in salesRoute

---
+++ routes/salesRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const salesRouter = express.Router();
+const controllers = require('../controllers');
+
+salesRouter.post('/', controllers.getAllSales);
+
+module.exports = salesRouter;
