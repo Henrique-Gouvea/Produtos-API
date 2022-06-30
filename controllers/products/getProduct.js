889af1ecc02ed3284d86518bd9d6a7886eb99bb5
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-06-30 14:17:46 -0300

    fix: change status error get product for 404

--- controllers/products/getProduct.js
+++ controllers/products/getProduct.js
@@ -1,14 +1,14 @@
 const services = require('../../services');
-const { OK, NOT_FOUND, INTERNAL_SERVER } = require('../../helpers/httpStatusCodes');
+const { OK, NOT_FOUND } = require('../../helpers/httpStatusCodes');
 
 const getProduct = async (req, res) => {
   try {
     const { id } = req.params;
-    const product = await services.getProduct(id);
+    const [product] = await services.getProduct(id);
     if (product.length === 0) return res.status(NOT_FOUND).json({ message: 'Product not found' });
-    return res.status(OK).json(product);
+    res.status(OK).json(product);
   } catch (error) {
-    return res.status(INTERNAL_SERVER).json({ message: 'Error getting' });
+    return res.status(NOT_FOUND).json({ message: 'Product not found' });
   }
 };
 
