4361dd2a70632fa4f3665e3ec550c2f37c205a5b
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-07-05 09:59:16 -0300

    feat: remove consoel log

--- controllers/sales/getSale.js
+++ controllers/sales/getSale.js
@@ -5,7 +5,6 @@ const getSale = async (req, res) => {
   try {
     const { id } = req.params;
     const sale = await services.getSale(id);
-    console.log(sale);
     if (sale.length === 0) return res.status(NOT_FOUND).json({ message: 'Sale not found' });
     res.status(OK).json(sale);
   } catch (error) {
--- services/sales/getSale.js
+++ services/sales/getSale.js
@@ -2,7 +2,6 @@ const models = require('../../models');
 
 const getSale = async (id) => {
   const sale = await models.getSale(id);
-  console.log(sale);
   if (!sale) return [];
   return sale;
 };
