6411c7707c2567bae2e94680e5e2fd099fa171ca
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-07-05 10:47:03 -0300

    fix: adjust lint un updateProducts controllers

--- controllers/products/updateProduct.js
+++ controllers/products/updateProduct.js
@@ -7,7 +7,10 @@ const updateProduct = async (req, res) => {
     const { name } = req.body;
     const productUpdated = await services.updateProduct(id, name);
     console.log(productUpdated);
-    if (productUpdated.length === 0) return res.status(NOT_FOUND).json({ message: 'Product not found' });
+    if (productUpdated.length === 0) {
+      return res.status(NOT_FOUND)
+        .json({ message: 'Product not found' });
+    }
     res.status(OK).json(productUpdated);
   } catch (error) {
     return res.status(NOT_FOUND).json({ message: 'Product not found' });
